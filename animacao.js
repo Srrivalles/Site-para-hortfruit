fdc81ad1d93dab9821a514c8b2c5392d1103c3bf
Srrivalles <matheuslimaandrade2102@gmail.com>
2024-06-13 00:05:46 -0400

    pim

---
+++ animacao.js
@@ -0,0 +1,9 @@
+/*Animação das imagens secundario escrito*/
+function ampliarImagem(elemento) {
+    elemento.classList.add('ampliada');
+  }
+  
+  function restaurarImagem(elemento) {
+    elemento.classList.remove('ampliada');
+  }
+/*Termino*/
