7c8fdb3d6020d32aade83a271b7ec52f8b21b972
Matteo <159387988+Srrivalles@users.noreply.github.com>
2025-05-13 14:31:33 -0400

    Delete animacao.js

--- animacao.js
+++
@@ -1,9 +0,0 @@
-/*Animação das imagens secundario escrito*/
-function ampliarImagem(elemento) {
-    elemento.classList.add('ampliada');
-  }
-  
-  function restaurarImagem(elemento) {
-    elemento.classList.remove('ampliada');
-  }
-/*Termino*/
